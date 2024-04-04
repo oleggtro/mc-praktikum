7236c78510c91a17761107bbae50a89cca25926e
old0847 <old0847@thi.de>
2024-04-04 12:21:09 +0200

    led lauflicht zick zack

--- Objects/_mcpr_stm32f407.d
+++ Objects/_mcpr_stm32f407.d
@@ -1,10 +1,10 @@
 ./objects/_mcpr_stm32f407.o: _mcpr_stm32f407.c _mcpr_stm32f407.h \
-  C:\bin\Arm\Packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h \
-  C:\bin\Arm\Packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f407xx.h \
-  C:\bin\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h \
-  C:\bin\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\bin\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
-  C:\bin\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
-  C:\bin\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
-  C:\bin\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
-  C:\bin\Arm\Packs\Keil\STM32F4xx_DFP\2.17.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
+  C:\bin\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h \
+  C:\bin\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f407xx.h \
+  C:\bin\Arm\CMSIS\5.9.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  C:\bin\Arm\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\bin\Arm\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\bin\Arm\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\bin\Arm\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
+  C:\bin\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
--- main.c
+++ main.c
@@ -3,17 +3,15 @@
 #include <inttypes.h>
 
 
-// switch modes between LEDs and LCD display
-#define run_mode 1
-#define show_mode 0
-uint8_t mode = show_mode;
-
 //Forward declaration
-void u_delay(uint32_t usec);
-void LEDs_InitPorts (void);
-void LCD_Output16BitWord (uint16_t data);
-void LEDs_Write (uint16_t data);
-void Run_LEDs (void);
+void u_delay(uint32_t umil);
+void LEDs_InitPorts(void);
+void LCD_Output16BitWord(uint16_t data);
+void LEDs_Write(uint16_t data);
+void Run_LEDs(void);
+
+
+const uint16_t delay = 50;
 
 
 //init ports
@@ -23,16 +21,16 @@ void LEDs_InitPorts(void)
     //Turn on peripheral GPIOD and GPIOE 
     RCC->AHB1ENR |= 1<<3| 1<<4| 1;
 
-    // GPIOD Konfiguration for LED selection and write signals
+    // GPIOD config for LED selection and write signals
     // configure PD7, PD11, PD5 as output
     GPIOD->MODER |= (1 << (7 * 2)) | (1 << (5 * 2)) | (1 << (11 * 2));
 
     // ensure PD7, PD11 and PD5 are set to high initially
     GPIOD->ODR |= (1 << 7) | (1 << 5) | (1 << 11) ;
 
-    // orange LED (D13) set output
-    GPIOD->MODER |= 1 << (12*2); // equiv to 0x04000000
-    GPIOD->ODR |= 1<<12;
+    //// orange LED (D13) set output
+    //GPIOD->MODER |= 1 << (12*2); // equiv to 0x04000000
+    //GPIOD->ODR |= 1<<12;
 
     // Configure data lines
     GPIOD->MODER |= (1 << (15*2))   | (1 << (9*2))  | (1 << (8*2)) | (1 << (10*2)) | (1 << (14*2))| (1 << (1*2)) | 1;
@@ -43,6 +41,33 @@ void LEDs_InitPorts(void)
 }
 
 
+
+
+
+
+// write data to register and activate LED running light
+void LEDs_Write(uint16_t data)
+{
+    // PD7 to low to address external hardware
+    GPIOD->ODR &= ~(1 << 7);
+
+    // PD11 to high: select ic for LEDs
+    GPIOD->ODR |= (1 << 11);
+
+    // write 16bit word to lcd
+    LCD_Output16BitWord(data);
+
+    // PD5 to high to freeze data
+    GPIOD->ODR |= (1 << 5);
+    u_delay(100);
+    // PD5 to low + then high to save data
+    GPIOD->ODR &= ~(1 << 5);
+    u_delay(10);
+    GPIOD->ODR |= (1 << 5);
+
+    // PD7 to high to deactivate external hardware
+    GPIOD->ODR |= (1 << 7);
+}
 
 // Function to write a 16-bit word to the LCD in the correct order
 void LCD_Output16BitWord(uint16_t data)
@@ -64,52 +89,23 @@ void LCD_Output16BitWord(uint16_t data)
     return;
 }
 
-
-// write data to register and activate LED running light
-void LEDs_Write(uint16_t data)
-{
-    // PD7 to low to address external hardware
-    GPIOD->ODR &= ~(1 << 7);
-
-    // PD11 to high: select ic for LEDs
-    GPIOD->ODR |= (1 << 11);
-
-    // write 16bit word to lcd
-    LCD_Output16BitWord(data);
-
-    // PD5 to high to freeze data
-    GPIOD->ODR |= (1 << 5);
-    // short delay (maybe a little to long)
-    u_delay(1);
-    // PD5 to low + then high to save data
-    GPIOD->ODR &= ~(1 << 5);
-    u_delay(1);
-    GPIOD->ODR |= (1 << 5);
-
-    // PD7 to high to deactivate external hardware
-    GPIOD->ODR |= (1 << 7);
-}
-
-
-// Funktion, um das Lauflicht zu steuern
 void Run_LEDs(void)
 {
-    // Lauflicht von Bit 0 bis 15
+	
     for(uint16_t i = 0; i < 16; i++) {
-        LEDs_Write(1 << i);  // Eine '1' durchschieben
-        u_delay(500000);     // 0,5 Sekunden warten
+        LEDs_Write(1 << i);  
+        u_delay(delay);     
     }
     
-    // Lauflicht von Bit 15 bis 0 mit einer '0'
     for(uint16_t i = 0; i < 16; i++) {
-        LEDs_Write(~(1 << i));  // Eine '0' durchschieben
-        u_delay(500000);        // 0,5 Sekunden warten
+        LEDs_Write(~(1 << i)); 
+        u_delay(delay);
     }
 }
 
 
-void u_delay(uint32_t secs) {
-	for (uint32_t x = 0; x < secs*3150000; x++);
+void u_delay(uint32_t umil) {
+	for (uint32_t x = 0; x < umil*3150; x++);
 }
 
 
@@ -123,26 +119,15 @@ int main(void)
 	LEDs_InitPorts();
 	
 	while( 1 ) {
-
-        // mode switch between lauflicht and lcd
-        if( mode == run_mode) {
-            // Lauflicht von Bit 0 bis 15
-            Run_LEDs();
-        }
-
-        else if ( mode == show_mode ) {
-            // 16bit display for LEDs
-            LEDs_Write(0xFFFF); // binary full on 16bit word
-        }
-
-		// test for button at A0 to activate LED D12 for 0.5s
+		
+    // LEDs_Write(0xFFFF); // binary full on 16bit word
+        
 		if( (GPIOA->IDR & 1) != 0) { 
-            u_delay(1); 
-			GPIOD->ODR |= 1<<12;
-            u_delay(1); 
+           Run_LEDs();
 		} else { 
-			GPIOD->ODR &= 0xEFFF; // ~(1<<12); 
+				LEDs_Write(0);
 		}
+		//u_delay(50);
         
 	}
     return 0;
